0b8b0ca1fc40f95026aeb9967b7be7e17da3ecf9
blindchimp <dwyco1@gmail.com>
2018-06-28 14:18:59 -0600

    fix some build problems

---
+++ vccmd/mkuv.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+SHADOW_NAME=$1
+
+mkdir $SHADOW_NAME
+mkdir $SHADOW_NAME/lib
+mkdir $SHADOW_NAME/include
+mkdir -p $SHADOW_NAME/bld/libuv
+(
+cd bld/libuv
+make -j 8
+cp libuv.a $SHADOW_NAME/bld/libuv
+cp include/uv.h $SHADOW_NAME/include
+)
+
+if [ ! -e $SHADOW_NAME/include/sp.h ]
+then
+( 
+cd bld/spread-hacked 
+cd `cat curver` 
+./configure --prefix=$SHADOW_NAME 
+make -j 8 install 
+) 
+fi
