fd04def1f6bbc52a517234d3e209b892e0239439
blindchimp <dwyco1@gmail.com>
2019-03-08 16:28:03 -0700

    fix x86 toolchain

--- randobld/jni/create_toolchain.sh
+++ randobld/jni/create_toolchain.sh
@@ -8,6 +8,6 @@ then
 $NDK/build/tools/make_standalone_toolchain.py --arch arm -v --api 19 --stl libc++ --install-dir ./toolchain
 else
 #$NDK/build/tools/make-standalone-toolchain.sh --toolchain=x86-4.8 --install-dir=./toolchain
-$NDK/build/tools/make_standalone_toolchain.py --arch x86 -v --api 19 --stl gnustl --install-dir ./toolchain
+$NDK/build/tools/make_standalone_toolchain.py --arch x86 -v --api 19 --stl libc++ --install-dir ./toolchain
 fi
 # --system=linux-x86
