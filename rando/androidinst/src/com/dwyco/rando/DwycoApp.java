265c7ffb55b3b8796ce6366baa465190631ec825
blindchimp <dwyco1@gmail.com>
2019-03-21 16:12:02 -0700

    add customization class DwycoApp

---
+++ rando/androidinst/src/com/dwyco/rando/DwycoApp.java
@@ -0,0 +1,9 @@
+package com.dwyco.android;
+import com.dwyco.rando.R;
+
+public class DwycoApp {
+    public static int notification_icon() {
+        return R.drawable.ic_stat_not_icon2;
+        }
+
+}
