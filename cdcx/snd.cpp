58385671587ec00a29c77b833f72878b7dca6f94
dwight <dwight@dwyco.com>
2020-05-16 07:33:47 -0700

    cruft removal

--- cdcx/snd.cpp
+++ cdcx/snd.cpp
@@ -6,98 +6,12 @@
 ; License, v. 2.0. If a copy of the MPL was not distributed with this file,
 ; You can obtain one at https://mozilla.org/MPL/2.0/.
 */
-#include <string.h>
+
 #include "dlli.h"
 #include "ssmap.h"
 #include "ct.h"
-#if 0 && defined(LINUX) && !defined(MAC_CLIENT)
-#include <SDL/SDL.h>
-#include <unistd.h>
-#include "esd.h"
-#include "dwstr.h"
-#include <QMap>
 
-// under linux, we'll just assume we have Esd for now
-//
-static int Esd = -1;
-static QMap<DwOString, int> SoundMap;
-
-void
-init_sound()
-{
-    Esd = esd_open_sound(0);
-    SoundMap.clear();
-}
-
-static void
-load_sound(const char *filename)
-{
-    Uint32 len;
-    Uint8 *buf;
-#ifndef MAC_CLIENT
-    SDL_AudioSpec spec;
-    if(SDL_LoadWAV(filename, &spec, &buf, &len) == 0)
-        return;
-
-    int ef = ESD_STREAM | ESD_PLAY;
-    if(Esd == -1)
-        return;
-    switch(spec.format)
-    {
-    case AUDIO_S8:
-    case AUDIO_U8:
-        ef |= ESD_BITS8;
-        break;
-    case AUDIO_S16:
-        ef |= ESD_BITS16;
-        break;
-
-    default:
-        return;
-    }
-    if(spec.channels == 1)
-        ef |= ESD_MONO;
-    else
-        ef |= ESD_STEREO;
-
-    int id = esd_sample_cache(Esd, ef, spec.freq, len, filename);
-    write(Esd, buf, len);
-    int id2 = esd_confirm_sample_cache(Esd);
-    if(id != id2)
-        return;
-    SoundMap.insert(filename, id);
-#endif
-}
-
-void
-play_sound_plat(const char *file)
-{
-    int id;
-    if(!SoundMap.contains(file))
-    {
-        load_sound(file);
-        if(!SoundMap.contains(file))
-            return;
-    }
-    id = SoundMap.value(file);
-    esd_sample_play(Esd, id);
-}
-
-void
-exit_sound()
-{
-    QList<int> v = SoundMap.values();
-    int n = v.count();
-    for(int i = 0; i < n; ++i)
-    {
-        esd_sample_free(Esd, v[i]);
-    }
-    SoundMap.clear();
-    close(Esd);
-    Esd = -1;
-}
-
-#elif defined(WIN32)
+#if defined(WIN32)
 #include <windows.h>
 void
 play_sound_plat(const char *fn)
