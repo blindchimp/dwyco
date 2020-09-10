6b4f553f8802245d8b13bb46db11921ab1689371
dwight <dwight@dwyco.com>
2020-09-09 20:20:44 -0600

    event logging for rando

--- rando/notificationclient.cpp
+++ rando/notificationclient.cpp
@@ -90,6 +90,7 @@ void NotificationClient::set_lastrun()
         "com/dwyco/android/NotificationClient",
         "set_lastrun");
 }
+
 void
 NotificationClient::set_msg_count_url(QString s)
 {
@@ -135,6 +136,21 @@ NotificationClient::log_event()
     QAndroidJniObject::callStaticMethod<void>(
         "com/dwyco/android/NotificationClient",
         "log_event"
+    );
+
+}
+
+void
+NotificationClient::log_event2(QString name, QString method)
+{
+    QAndroidJniObject jname = QAndroidJniObject::fromString(name);
+    QAndroidJniObject jval = QAndroidJniObject::fromString(method);
+    QAndroidJniObject::callStaticMethod<void>(
+        "com/dwyco/android/NotificationClient",
+        "log_event2",
+      "(Ljava/lang/String;Ljava/lang/String;)V",
+      jname.object<jstring>(),
+      jval.object<jstring>()
     );
 
 }
