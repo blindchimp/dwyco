3dc4ec52d3ce8935fcd530c7cfcc922e7caffccf
blindchimp <dwyco1@gmail.com>
2021-10-21 14:39:50 -0600

    fix bug in remove_user
    oops, c++ mystery conversion operators strike again

--- bld/cdc32/dlli.cpp
+++ bld/cdc32/dlli.cpp
@@ -4064,7 +4064,7 @@ dwyco_delete_user(const char *uid, int len_uid)
 
     Rescan_msgs = 1;
     //vc dir = uid_to_dir(u);
-    int ret = remove_user(uid, "");
+    int ret = remove_user(u, "");
     ack_all(u);
     pal_del(u, 1);
     prf_invalidate(u);
@@ -4080,7 +4080,7 @@ dwyco_clear_user(const char *uid, int len_uid)
 
     Rescan_msgs = 1;
     //vc dir = uid_to_dir(u);
-    int ret = clear_user(uid, "");
+    int ret = clear_user(u, "");
     ack_all(u);
     return ret;
 }
