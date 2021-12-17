35e0e09ce4d398abc9ce4a6b41cd79d72dac96a6
dwight <dwight@dwyco.com>
2021-12-17 12:51:32 -0700

    fix pal list upgrade

--- bld/cdc32/qmsg.cc
+++ bld/cdc32/qmsg.cc
@@ -655,8 +655,15 @@ resume_qmsg()
     }
     if(!Pals.is_nil())
     {
-        pals_to_tags();
-        save_info(vcnil, "pals");
+        // compat hack. don't obliterate their old pal list
+        // leave it intact so the old software doesn't crash
+        // if they downgrade.
+        vc loaded_pals;
+        if(!load_info(loaded_pals, "loadpals.dif") || loaded_pals.is_nil())
+        {
+            pals_to_tags();
+            save_info(vctrue, "loadpals.dif");
+        }
     }
     Pals = get_local_pals();
     Client_ports = vc(VC_TREE);
