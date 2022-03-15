0dfa3c61e713eb3fcc8872c36ecab47156dbc3e7
blindchimp <dwyco1@gmail.com>
2022-03-15 11:21:10 -0600

    phoo should allow screen off

--- phoo/androidinst2/src/com/dwyco/phoo/DwycoApp.java
+++ phoo/androidinst2/src/com/dwyco/phoo/DwycoApp.java
@@ -35,6 +35,6 @@ public class DwycoApp {
 
         final public static boolean is_rando = false;
 
-	final public static boolean keep_screen_on = true;
+	final public static boolean keep_screen_on = false;
 	
 }
