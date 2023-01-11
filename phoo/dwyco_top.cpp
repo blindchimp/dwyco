223baf8b7f19841c6bad4185063ed8dae87e6412
dwightm <dwight@dwyco.com>
2023-01-11 14:05:00 -0700

    wip
    try moveToThread for adding objects. seems to work ok to overcome qt's thread affinity issues. at least for chat server.

--- phoo/dwyco_top.cpp
+++ phoo/dwyco_top.cpp
@@ -618,8 +618,8 @@ DwycoCore::emit_chat_event(int cmd, int id, const char *uid, int len_uid, const 
                 int type, const char *val, int len_val,
                 int qid, int extra_arg)
 {
-    if(DwycoCore::Suspended)
-        return;
+//    if(DwycoCore::Suspended)
+//        return;
     QByteArray buid(uid, len_uid);
     QString huid = buid.toHex();
     QString sname(QByteArray(name, len_name));
@@ -2113,7 +2113,7 @@ DwycoCore::app_state_change(Qt::ApplicationState as)
     {
         Suspended = 1;
         simple_call::suspend();
-        dwyco_disconnect_chat_server();
+        //dwyco_disconnect_chat_server();
         dwyco_suspend();
         if(BGLockSock)
         {
