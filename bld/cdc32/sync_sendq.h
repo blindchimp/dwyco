a9cf6adc6c6dd84a903630daab5532be1eb7282b
dwightm <dwight@dwyco.com>
2023-08-15 15:19:33 -0600

    docs

--- bld/cdc32/sync_sendq.h
+++ bld/cdc32/sync_sendq.h
@@ -5,6 +5,35 @@
 
 namespace dwyco {
 
+// this implements a simple queue that is used to send sync
+// commands to a remote client. the idea is that as you are
+// browsing your messages, "pull" requests are generated to other
+// clients if you do not have the message locally. the queue also
+// accepts "pull-resp" and "idx" messages.
+// commands are queued FIFO, with a few twists:
+// you can give a priority to the item, and all items with
+// a given priority are queued together. this is used in cases where
+// a "pull" command is generated through some path that is interactive (ie,
+// the user wants to view a message he doesn't have.) in that case,
+// the command should be processed before other commands that are
+// "background" in nature (like pulling in eager mode.) in addition, the
+// response to the pull on the remote side (pull-resp) should be
+// higher priority as well. this allows somewhat better response times
+// for interactive pulls.
+// there are some minor complications: if there is a pull already
+// queued with a lower priority, you can increase the priority if it
+// becomes the target of an interactive action. NOTE: once a command is
+// issued to the network, you can't adjust anything...it is getting sent
+// and the response on the remote side will have that lower priority, and
+// might get "stuck" behind a bunch of new higher priority requests.
+//
+// note: this queue is really only exercised in some specific cases...
+// if you have a large corpus of messages that you want to sync
+// eagerly to a new device that is largely empty, and you need to
+// browse the messages while this operation is in progress. once
+// the corpus is mostly copied, the size of this queue is tiny, and
+// is probably total overkill.
+//
 class sendq {
 
     struct qkey {
