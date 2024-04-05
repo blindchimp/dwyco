e7975f5702fecdd7b6d75d7f2e19c07c4fe39caa
dwightm <dwight@dwyco.com>
2024-04-04 18:16:33 -0700

    fuzzer updates

--- fuzz/fuzz.cpp
+++ fuzz/fuzz.cpp
@@ -11,12 +11,19 @@ int Continue_on_alarm;
 void
 oopanic(const char *s)
 {
-    ::abort();
+	// note: this isn't a "crash" from afl perspective,
+	// since we detected it rather than segv or something.
+	// note that a server might still crash, and this is
+	// worth trying to fix, if a stream comes in with
+	// sets as "keys" or something, since that makes no
+	// sense.
+    exit(0);
 }
 
 int
 main(int, char **argv)
 {
+	vc::set_xferin_constraints(20000, -1, -1, -1);
 	vc v(VC_FILE);
 	v.open(argv[1], VCFILE_READ|VCFILE_BINARY);
 	vcxstream vcx(v);
@@ -24,6 +31,7 @@ main(int, char **argv)
 
 	vc o;
 	o.xfer_in(vcx);
-    o.print_top(VcOutput);
+    //o.print_top(VcOutput);
+exit(0);
 }
 
