62a365091deb466c950df839736eac02993bd420
dwightm <dwight@dwyco.com>
2024-06-22 17:38:09 -0600

    cmake, builds but doesn't run
    there is something wrong with the qml file resources

--- rando/subdirs.cmake
+++ rando/subdirs.cmake
@@ -18,6 +18,8 @@ add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/pbm ${CMAKE_CURRENT_BINARY_DIR}/bld/
 add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/cdc32 ${CMAKE_CURRENT_BINARY_DIR}/bld/cdc32)
 add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/uv ${CMAKE_CURRENT_BINARY_DIR}/bld/uv)
 add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/vc ${CMAKE_CURRENT_BINARY_DIR}/bld/vc)
+add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/qt-qml-models ${CMAKE_CURRENT_BINARY_DIR}/bld/qt-qml-models)
+add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/jhead ${CMAKE_CURRENT_BINARY_DIR}/bld/jhead)
 #add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/miniupnp/miniupnp-master/miniupnpc ${CMAKE_CURRENT_BINARY_DIR}/bld/miniupnp/miniupnp-master/miniupnpc)
 
 #if(UNIX AND NOT APPLE)
