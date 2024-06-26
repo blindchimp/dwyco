a858a7f9edb66b4fa4541706a13813caf06a8a0c
dwightm <dwight@dwyco.com>
2024-06-26 09:51:43 -0600

    cmake

--- rando/subdirs.cmake
+++ rando/subdirs.cmake
@@ -1,31 +1,16 @@
-# note: this is a kluge, hoping that zlib gets created first since it screws
-# around and creates some header files at build time (whoever decided this
-# was a good thing... sheesh)
-#add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/zlib ${CMAKE_CURRENT_BINARY_DIR}/bld/zlib)
+
+if(NOT ANDROID)
 add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/crypto5 ${CMAKE_CURRENT_BINARY_DIR}/bld/crypto5)
 add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/kazlib ${CMAKE_CURRENT_BINARY_DIR}/bld/kazlib)
 add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/jenkins ${CMAKE_CURRENT_BINARY_DIR}/bld/jenkins)
 add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/dwcls ${CMAKE_CURRENT_BINARY_DIR}/bld/dwcls)
-#add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/ogg ${CMAKE_CURRENT_BINARY_DIR}/bld/ogg)
-#add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/vorbis112 ${CMAKE_CURRENT_BINARY_DIR}/bld/vorbis112)
-#add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/theora.1.2.x ${CMAKE_CURRENT_BINARY_DIR}/bld/theora.1.2.x)
-#add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/speex ${CMAKE_CURRENT_BINARY_DIR}/bld/speex)
-
 add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/pbm ${CMAKE_CURRENT_BINARY_DIR}/bld/pbm)
-#add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/pgm ${CMAKE_CURRENT_BINARY_DIR}/bld/pgm)
-#add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/ppm ${CMAKE_CURRENT_BINARY_DIR}/bld/ppm)
-#add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/gsm ${CMAKE_CURRENT_BINARY_DIR}/bld/gsm)
 add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/cdc32 ${CMAKE_CURRENT_BINARY_DIR}/bld/cdc32)
 add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/uv ${CMAKE_CURRENT_BINARY_DIR}/bld/uv)
 add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/vc ${CMAKE_CURRENT_BINARY_DIR}/bld/vc)
-add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/qt-qml-models ${CMAKE_CURRENT_BINARY_DIR}/bld/qt-qml-models)
 add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/jhead ${CMAKE_CURRENT_BINARY_DIR}/bld/jhead)
+endif()
+add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/qt-qml-models ${CMAKE_CURRENT_BINARY_DIR}/bld/qt-qml-models)
+
 #add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/miniupnp/miniupnp-master/miniupnpc ${CMAKE_CURRENT_BINARY_DIR}/bld/miniupnp/miniupnp-master/miniupnpc)
 
-#if(UNIX AND NOT APPLE)
-#add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/qt6drv ${CMAKE_CURRENT_BINARY_DIR}/bld/qt6drv)
-#add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/v4lcap ${CMAKE_CURRENT_BINARY_DIR}/bld/v4lcap)
-#endif()
-#if(APPLE OR WIN32 OR MSVC)
-#add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/qt6drv ${CMAKE_CURRENT_BINARY_DIR}/bld/qt6drv)
-#endif()
