4410b1576fa525e2bed65104813da2a7463c22bf
dwightm <dwight@dwyco.com>
2024-06-30 10:27:06 -0600

    cruft removal, android build

--- randobld/jni/Application.mk
+++ randobld/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 APP_PROJECT_PATH := $(HOME)/git/dwyco/randobld
-include $(APP_PROJECT_PATH)/dwyco_android.mk
+DWYCO_APP := "rando"
 APP_CPPFLAGS += -frtti -fexceptions -DANDROID -std=c++11
 APP_CFLAGS += -DANDROID 
 APP_STL := c++_shared
