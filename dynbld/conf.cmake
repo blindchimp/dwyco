be9c75fa3fc4903f905468000271d03b32d24844
dwight <dwight@dwyco.com>
2024-11-21 09:25:38 -0700

    switch to OBJECT libraries from STATIC
    appears to build a linux .so file ok. links with dwycobg fine.

--- dynbld/conf.cmake
+++ dynbld/conf.cmake
@@ -3,31 +3,31 @@ set(DWYCO_APP "dynlib")
 set(CMAKE_CXX_COMPILER_LAUNCHER ccache)
 add_compile_definitions(VCCFG_FILE DWYCO_APP_NICENAME="dwyco-generic")
 set(VCCFG_COMP ${CMAKE_CURRENT_SOURCE_DIR}/../../${DWYCO_CONFDIR})
+add_compile_definitions( CRYPTOPP_MANUALLY_INSTANTIATE_TEMPLATES)
 # Determine the host platform
 if(UNIX)
-	add_definitions(-DLINUX)
+        add_compile_definitions(LINUX)
         add_compile_options(-fPIC -Wall -Wno-unused-parameter -Wno-reorder -Wno-unused-variable -Wno-unused-function)
 endif()
 
 if(WIN32)
-add_definitions(-D_WIN32 -D__WIN32__ -D_Windows)
+add_compile_definitions(_WIN32 __WIN32__ _Windows)
 add_compile_options(/wd4100 /wd4068 /wd4189 /wd4291)
 endif()
 
 if(APPLE)
-message("FUCK")
     # Set compiler definitions for macOS
-    add_definitions(-DMACOSX)
+    add_compile_definitions(MACOSX)
 endif()
 
 
 if(ANDROID)
-    add_definitions(-DANDROID)
+    add_compile_definitions(ANDROID)
     add_compile_options( -frtti -fexceptions)
 endif()
 
 # Handle Emscripten-Specific Settings
 if(EMSCRIPTEN)
-    add_definitions(-DEMSCRIPTEN)
+    add_compile_definitions(EMSCRIPTEN)
 endif()
 
