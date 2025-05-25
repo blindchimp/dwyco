b215a7c029a920f5c630ed05334a6cab655873c9
dwight <dwight@dwyco.com>
2025-05-25 11:39:37 -0600

    build cdcx with cmake

---
+++ cdcxbld/cmake-build-mac.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+QTVERS=6.8.2
+export PATH=~/Qt/$QTVERS/macos/bin:$PATH
+export CMAKE_CXX_COMPILER_LAUNCHER=ccache
+
+~/Qt/$QTVERS/macos/bin/qt-cmake -D QT_QMAKE_EXECUTABLE="/Users/dwight/Qt/$QTVERS/macos/bin/qmake" -D CMAKE_BUILD_TYPE=Release -D CMAKE_OSX_ARCHITECTURES="arm64;x86_64" -S ~/git/dwyco/cdcx -B ~/git/build-cdcx -G Ninja
+#~/Qt/6.8.2/macos/bin/qt-cmake --build ~/git/build-phoo --target all
+
+cd ~/git/build-cdcx
+#make -j 8
+ninja
+
