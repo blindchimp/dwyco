812fd060f2df84853e5b296ef5450fb1ee2b6e01
dwight <dwight@dwyco.com>
2025-05-28 15:42:28 -0600

    deploy build script for linux
    needs testing with qt6 deployment

---
+++ cdcxbld/cmake-build-linux.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# since this is the file we use to deploy, go ahead and
+# update the buildtime
+(cd ~/git/dwyco/cdcx;./dumptime)
+
+QTVERS=6.8.2
+QTFLAVOR=gcc_64
+export PATH=~/Qt/$QTVERS/$QTFLAVOR/bin:$PATH
+export CMAKE_CXX_COMPILER_LAUNCHER=ccache
+
+~/Qt/$QTVERS/$QTFLAVOR/bin/qt-cmake -D CMAKE_BUILD_TYPE=Release -S ~/git/dwyco/cdcx -B ~/git/build-cdcx -G Ninja
+#~/Qt/$QTVERS/$QTFLAVOR/bin/qt-cmake -D QT_QMAKE_EXECUTABLE="/Users/dwight/Qt/$QTVERS/$QTFLAVOR/bin/qmake" -D CMAKE_BUILD_TYPE=Release -S ~/git/dwyco/cdcx -B ~/git/build-cdcx -G Ninja
+
+cd ~/git/build-cdcx
+ninja
+
