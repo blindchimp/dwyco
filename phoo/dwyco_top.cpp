e47b6b0086741dff2739275042aa41c5d01d24e9
dwight <dwight@dwyco.com>
2025-08-07 08:29:31 -0600

    fix phoo emergency server stuff

--- phoo/dwyco_top.cpp
+++ phoo/dwyco_top.cpp
@@ -1988,6 +1988,7 @@ DwycoCore::init()
 
     if(!dwyco_init())
         ::abort();
+    setup_emergency_servers();
     Init_ok = 1;
     dwyco_set_setting("zap/always_server", "0");
     dwyco_set_setting("call_acceptance/auto_accept", "0");
@@ -2131,8 +2132,6 @@ DwycoCore::init()
     }
 
     update_android_backup_available(dwyco_get_android_backup_state());
-    setup_emergency_servers();
-
 }
 
 void
