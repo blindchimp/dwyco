1bc12ba20b057b16f930bcf298884271eaab1070
blindchimp <dwyco1@gmail.com>
2025-08-27 16:27:45 -0600

    hotfix for cdcx
    move up to qt 6.9.2 and change the video selection to be a little less aggressive in no-camera mode

--- cdcxbld/cmake-build-win.bat
+++ cdcxbld/cmake-build-win.bat
@@ -1,6 +1,6 @@
 echo on
 
-set PATH=c:\qt\6.8.2\msvc2022_64\bin;%PATH%
+set PATH=c:\qt\6.9.2\msvc2022_64\bin;%PATH%
 
 
 cd "\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build"
@@ -17,7 +17,7 @@ cd %SHADOW_NAME%
 %D%\dwyco\cdcx\dumptime %D%\dwyco\cdcx\main.cpp %D%\dwyco\cdcx\buildtime.h
 
 
-set QTVERS=6.8.2
+set QTVERS=6.9.2
 set QTFLAVOR=msvc2022_64
 
 c:/Qt/%QTVERS%/%QTFLAVOR%/bin/qt-cmake -D QT_QMAKE_EXECUTABLE="\Qt\%QTVERS%\%QTFLAVOR%\bin\qmake" -D CMAKE_BUILD_TYPE=Release -S %D%/dwyco/cdcx -B %D%/build-cdcx -G Ninja
