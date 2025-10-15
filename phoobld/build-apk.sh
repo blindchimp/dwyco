286e90811ba27bab3fe51513df26114659377ed7
dwight <dwight@dwyco.com>
2025-10-15 11:21:37 -0600

    minor build tweaks

--- phoobld/build-apk.sh
+++ phoobld/build-apk.sh
@@ -1,6 +1,7 @@
 # this script just builds one debug apk for rando
 # this uses the 16kb version of qt 6.8.2 build with ndk 28.2
 QTDIR=$HOME/qt682
+rm -rf /tmp/r
 
 #$HOME/syncdev/qta682/android682_arm64_v8a/bin/qt-cmake -S $HOME/git/dwyco/phoo -B /tmp/r -GNinja 
 $HOME/syncdev/qta682/android682_x86_64/bin/qt-cmake   \
