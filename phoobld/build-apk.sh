32d340ee367cf20ef228669bfaed87345ed6e0a0
dwight <dwight@dwyco.com>
2026-01-02 13:55:17 -0700

    move phoo to 6.10.1

--- phoobld/build-apk.sh
+++ phoobld/build-apk.sh
@@ -1,9 +1,10 @@
 # this script just builds one debug apk for rando
 # this uses the 16kb version of qt 6.8.2 build with ndk 28.2
-QTDIR=$HOME/qt682
+QTDIR=$HOME/qt6101
 rm -rf /tmp/r
 
-#$HOME/syncdev/qta682/android682_arm64_v8a/bin/qt-cmake -S $HOME/git/dwyco/phoo -B /tmp/r -GNinja 
-$HOME/syncdev/qta682/android682_x86_64/bin/qt-cmake   \
+$HOME/syncdev/qta6101/android6101_arm64-v8a/bin/qt-cmake \
 -D QT_QMAKE_EXECUTABLE="$QTDIR/bin/qmake" \
+-D CMAKE_BUILD_TYPE=Release \
+-D QT_ANDROID_SIGN_APK=ON \
 -S $HOME/git/dwyco/phoo -B /tmp/r -GNinja 
