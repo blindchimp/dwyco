76eab3135fab32f11386528c5ab997ef33dc74a8
dwight <dwight@dwyco.com>
2026-02-16 13:51:29 -0700

    retire self-compiled qt6.10.1
    use qt 6.10.2 from qt. note must use ndk28+ when compiling to get 16kb everything.

--- randobld/build-apk.sh
+++ randobld/build-apk.sh
@@ -1,10 +1,14 @@
 # this script just builds one debug apk for rando
 # this uses the 16kb version of qt 6.8.2 build with ndk 28.2
-QTDIR=$HOME/qt6101
+QTDIR=$HOME/Qt/6.10.2/android_x86_64
+export PATH=$QTDIR/bin:$PATH
 rm -rf /tmp/r
 
-$HOME/syncdev/qta6101/android6101_arm64-v8a/bin/qt-cmake \
+qt-cmake \
 -D QT_QMAKE_EXECUTABLE="$QTDIR/bin/qmake" \
--D CMAKE_BUILD_TYPE=Release \
--D QT_ANDROID_SIGN_APK=ON \
+-D ANDROID_SDK_ROOT=$HOME/Android/Sdk \
+-D ANDROID_NDK_ROOT=$HOME/Android/Sdk/ndk/28.2.13676358 \
 -S $HOME/git/dwyco/rando -B /tmp/r -GNinja 
+
+#-D CMAKE_BUILD_TYPE=Release \
+#-D QT_ANDROID_SIGN_APK=ON \
