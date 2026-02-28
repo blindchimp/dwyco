1a225fd3308d9e354b5cc5ccb1b47d78317145d3
dwight <dwight@dwyco.com>
2026-02-28 15:38:31 -0700

    manual hack to avoid autoconf

--- bld/libogg-1.3.3/include/ogg/os_types.h
+++ bld/libogg-1.3.3/include/ogg/os_types.h
@@ -141,7 +141,13 @@
 
 #else
 
-#  include <ogg/config_types.h>
+#  include <sys/types.h>
+   typedef int16_t ogg_int16_t;
+   typedef u_int16_t ogg_uint16_t;
+   typedef int32_t ogg_int32_t;
+   typedef u_int32_t ogg_uint32_t;
+   typedef int64_t ogg_int64_t;
+//#  include <ogg/config_types.h>
 
 #endif
 
