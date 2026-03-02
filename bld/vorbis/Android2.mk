dc19a55417ae600597ff324a82a41ae2796e1157
dwight <dwight@dwyco.com>
2026-03-02 16:25:28 -0700

    retire more old vorbis

--- bld/vorbis/Android2.mk
+++
@@ -1,8 +0,0 @@
-
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libvorbis-prebuilt
-LOCAL_SRC_FILES := output/lib/libvorbis.a
-include $(PREBUILT_STATIC_LIBRARY)
-
--- bld/vorbis/Android3.mk
+++
@@ -1,6 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libvorbisenc-prebuilt
-LOCAL_SRC_FILES := output/lib/libvorbisenc.a
-include $(PREBUILT_STATIC_LIBRARY)
