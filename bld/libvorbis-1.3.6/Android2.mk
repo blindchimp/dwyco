d3d0a63d07303227766923fc2f3dd58342fbfe93
dwight <dwight@dwyco.com>
2026-03-02 16:22:46 -0700

    retire old libvorbis

--- bld/libvorbis-1.3.6/Android2.mk
+++
@@ -1,8 +0,0 @@
-
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libvorbis-prebuilt
-LOCAL_SRC_FILES := $(APP_PROJECT_PATH)/obj/local/$(TARGET_ARCH_ABI)/libvorbis.a
-include $(PREBUILT_STATIC_LIBRARY)
-
