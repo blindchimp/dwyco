189bd3be4497b2cf083ac381faa44c29815384c5
dwight <dwight@dwyco.com>
2026-03-02 15:52:35 -0700

    retire old versions of speexdsp

--- bld/speex/libspeex/config-wtf.h
+++
@@ -1,4 +0,0 @@
-// i think you need to include this as "config.h" for the
-// BDS compile, but i'm not sure. have to check it out.
-#define inline
-#define restrict
--- bld/speex/win32/rad2007/config.h
+++
@@ -1,8 +0,0 @@
-// codegear rad studio 2007
-#define inline
-#define restrict
-
-#define DISABLE_WARNINGS
-#define EXPORT
-#define FLOATING_POINT
-#define USE_SMALLFT
