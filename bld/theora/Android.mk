4f8a75d22a1aa5ca5d8b964ab02026a2f87397a2
dwight <dwight@dwyco.com>
2026-03-02 13:41:49 -0700

    retire old theora lib

--- bld/theora/Android.mk
+++
@@ -1,6 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libtheora-prebuilt
-LOCAL_SRC_FILES := output/lib/libtheora.a
-include $(PREBUILT_STATIC_LIBRARY)
--- bld/theora/Android3.mk
+++
@@ -1,8 +0,0 @@
-
-
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libtheoraenc-prebuilt
-LOCAL_SRC_FILES := output/lib/libtheoraenc.a
-include $(PREBUILT_STATIC_LIBRARY)
