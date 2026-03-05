1ab657abc1e834375e152dba49303f7271423f35
dwight <dwight@dwyco.com>
2026-03-05 14:27:23 -0700

    android rando build use new cmake build

--- rando/subdirs.cmake
+++ rando/subdirs.cmake
@@ -8,9 +8,22 @@ add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/pbm ${CMAKE_CURRENT_BINARY_DIR}/bld/
 add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/cdc32 ${CMAKE_CURRENT_BINARY_DIR}/bld/cdc32)
 add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/uv ${CMAKE_CURRENT_BINARY_DIR}/bld/uv)
 add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/vc ${CMAKE_CURRENT_BINARY_DIR}/bld/vc)
-add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/jhead ${CMAKE_CURRENT_BINARY_DIR}/bld/jhead)
 endif()
+add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/jhead ${CMAKE_CURRENT_BINARY_DIR}/bld/jhead)
 add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/qt-qml-models ${CMAKE_CURRENT_BINARY_DIR}/bld/qt-qml-models)
+# this would be nice, but doesn't work. there is something bleeding in from the surrounding
+# Qt cmake stuff that is making the android api level in the NDK build wrong.
+#
+# this thing is not dependent on qt in any way.
+# maybe i'll figure it out eventually...
+#
+# in the mean time, you'll just have to invoke cmake by hand using the shell scripts in
+# rando_dynbld
+#
+#if(ANDROID)
+#add_subdirectory(${CMAKE_SOURCE_DIR}/../rando_dynbld ${CMAKE_CURRENT_BINARY_DIR}/rando_dynbld)
+#endif()
+
 
 #add_subdirectory(${CMAKE_SOURCE_DIR}/../bld/miniupnp/miniupnp-master/miniupnpc ${CMAKE_CURRENT_BINARY_DIR}/bld/miniupnp/miniupnp-master/miniupnpc)
 
